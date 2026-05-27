381eb0707db35d166c167344ba33e4d5508d23a5
Willian Pessoa <willian.pessoa.cs@gmail.com>
2026-05-26 22:09:17 -0300

    feat: implement process module

--- src/process.c
+++ src/process.c
@@ -1,1 +1,25 @@
 #include "cutesim/process.h"
+
+#include <stdlib.h>
+
+Process *process_create(int pid, int arrival_tick, int creation_seq) {
+    Process *p = calloc(1, sizeof(Process));
+    if (!p) {
+        return NULL;
+    }
+    p->pid            = pid;
+    p->arrival_tick   = arrival_tick;
+    p->creation_seq   = creation_seq;
+    p->status         = PROC_READY;
+    p->first_cpu_tick = -1;
+    p->completion_tick = -1;
+    return p;
+}
+
+void process_destroy(Process *p) {
+    if (!p) {
+        return;
+    }
+    free(p->io_script);
+    free(p);
+}
